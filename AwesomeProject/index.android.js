8f346e5059c64a8119c208c854f10d3f046e7d0e
Julian Szczurowski <Julian.Szczurowski@fabres.pl>
2017-07-31 11:24:44 +0200

    trying to use the CamComponent

--- AwesomeProject/index.android.js
+++ AwesomeProject/index.android.js
@@ -1,36 +1,6 @@
-import React, { Component } from 'react';
-import { Alert, AppRegistry, View, Text, Button } from 'react-native';
-import { Camera } from 'react-native-camera';
-import message from './wickedService';
-import CamComponent from './app/camComponent'
+import React from 'react';
+import { AppRegistry } from 'react-native';
 
-export default class FlexDirectionBasics extends Component {
-  _onPressButton() {
-    Alert.alert('Button pressed');
-  }
+import Example from './Example';
 
-  _onBarCodeRead(data) {
-    Alert.alert(JSON.stringify(data));
-  }
-
-  render() {
-    return (
-      // Try setting `flexDirection` to `column`.
-      <View style={{flex: 1, flexDirection: 'column', justifyContent: 'space-between', alignItems: 'center'}}>
-        <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}} />
-        <View style={{width: 50, height: 50, backgroundColor: 'steelblue'}} />
-        <View style={{width: 50, height: 50, backgroundColor: 'red'}} />
-        <View style={{width: 350, height: 50, backgroundColor: 'powderblue', flexDirection:'row'}}>
-          <Text style={{fontSize: 40, borderColor: 'green', borderWidth:2}}>{message}</Text>
-          <Button onPress={this._onPressButton} title='press it bitch' />
-        </View>
-        <View style={{width: 350, height: 50, backgroundColor: 'powderblue', flexDirection:'row'}}>
-          <CamComponent onBarCodeRead={_onBarCodeRead} />
-        </View>
-      </View>
-    );
-  }
-};
-
-// skip this line if using Create React Native App
-AppRegistry.registerComponent('AwesomeProject', () => CamComponent);
+AppRegistry.registerComponent('Example', () => Example);
